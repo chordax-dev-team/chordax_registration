ae9795ea544c2b244da095d16edceb0e4407a9d0
sb17018 <piotr.a.bar@gmail.com>
2024-11-11 21:18:50 +0100

    Initialize project

---
+++ chordax_registration/src/main/java/chordax_dev_team/chordax_registration/ChordaxRegistrationApplication.java
@@ -0,0 +1,13 @@
+package chordax_dev_team.chordax_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordaxRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChordaxRegistrationApplication.class, args);
+	}
+
+}
---
+++ chordax_registration/src/test/java/chordax_dev_team/chordax_registration/ChordaxRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package chordax_dev_team.chordax_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordaxRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
