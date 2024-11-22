9ecdcc2d4eb8872e57affeefe70fbd8796e20c07
sb17018 <piotr.a.bar@gmail.com>
2024-11-22 05:35:48 +0100

    Amend ServiceUser files

--- chordax_registration/src/main/java/chordax_dev_team/chordax_registration/serviceuser/model/ServiceUserRepository.java
+++
@@ -1,7 +0,0 @@
-package chordax_dev_team.chordax_registration.serviceuser.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ServiceUserRepository extends CrudRepository<ServiceUser, Long>{
-	ServiceUser findByEmail(String email);
-}
